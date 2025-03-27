7b1cb557276b053f2da62a4dfb0ee33e0c1a271d
Abdunesr <nesredinabdelah33@gmail.com>
2025-03-27 16:00:17 +0300

    adding all neccessary files for my project like model

---
+++ app/Models/FuelType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+   /*  protected $table='car_fuel-types';
+    protected $primaryKey='fuel_type-id';
+    public $keyType='string';
+    const UPDATED_AT=null;
+    const CREATED_AT=null;
+     */
+    public $timestamps=false;
+
+}
+
